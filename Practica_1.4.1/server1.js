90df1eac38882993babba0bfd55c7d1ee943013c
AdanM1509 <221K0044@itscarrillopuerto.edu.mx>
2026-02-27 02:16:16 -0500

    Practica 1.4 - Node y Express

---
+++ Practica_1.4.1/server1.js
@@ -0,0 +1,19 @@
+/* Adan Adair Moo Noh 8B */
+
+/* AA 1.4 Framework JS */
+
+
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
